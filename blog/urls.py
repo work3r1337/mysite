b4a5a0f659cac834ad1b4656357b21f673f251e1
work3r1337 <c.michasov2004@yandex.ru>
2025-06-20 13:27:38 +0300

    Add canonical url

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     # представления поста
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
+         views.post_detail,
+         name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 
@@ -9,13 +9,13 @@ def post_list(request):
                   'blog/post/list.html',
                   {'posts': posts})
 
-def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-
-    except Post.DoesNotExist:
-        raise Http404('Post Not Found')
-
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=slug,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
